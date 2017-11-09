82b01e23aedca742ee8f2da7b124df2dc13ec0ef
Alejandro Villarreal <avillarreal01@gmail.com>
2017-11-09 12:22:01 -0500

    feat(models): add models directory:

--- src/index.js
+++ src/index.js
@@ -3,17 +3,27 @@ const fs = require('fs')
 const Model = require('./models')
 
 const operator = (buildPath, data) => {
-  // Write seperate file for each key of input
-  Object.keys(data).forEach(key => {
-    const model = new Model(key, data[key])
-    writeFile(buildPath, model.filename, model.output)
-  })
+  // Create /destination directory
+  if (!fs.existsSync(buildPath)) {
+    fs.mkdirSync(buildPath)
+  }
+
+  // Create array of Models
+  const models = Object.keys(data.models).map(
+    key => new Model(key, data.models[key])
+  )
+
+  // Write file for each model in /models directory
+  models.forEach(model =>
+    writeFile(buildPath + '/models', model.filename, model.output)
+  )
 }
 
 const writeFile = (buildPath, fileName, content) => {
   if (!fs.existsSync(buildPath)) {
     fs.mkdirSync(buildPath)
   }
+
   fs.writeFile(`${buildPath}/${fileName}`, content, err => {
     if (err) console.log(err)
   })
